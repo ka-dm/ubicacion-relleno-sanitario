a8e5a090a8a202de7cb0a44bc33c8a545eb08c6d
kevin <kevindm181998@gmail.com>
2022-01-11 20:54:44 -0500

    gui style

--- RellenoGUI/View.py
+++ RellenoGUI/View.py
@@ -8,8 +8,7 @@ class View(ttk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
 
-        ttk.Style().configure("TButton", padding=6, relief="flat",
-        background="#1978f5")
+        ttk.Style().configure("TButton", padding=6, relief="flat",)
 
         # create widgets
         
@@ -19,7 +18,11 @@ class View(ttk.Frame):
         
         # labe frame parametros
         self.label_frame = ttk.LabelFrame(self, text='Parametros')
-        self.label_frame.grid(column=0, row=0, padx=0)
+        self.label_frame.grid(column=0, row=0, padx=0, pady=5, columnspan=2, sticky='nsew')
+        
+        # labe frame coordenadas
+        self.lb_coordenadas = ttk.LabelFrame(self.label_frame, text='Coordeandas')
+        self.lb_coordenadas.grid(column=0, row=1, padx=0, pady=5, columnspan=2, sticky='nsew')
 
         # message
         self.message_label = ttk.Label(self, text='', foreground='red')
@@ -32,32 +35,37 @@ class View(ttk.Frame):
         # tamaño area entry
         self.tam_area_var = tk.StringVar()
         self.tam_area_entry = ttk.Entry(self.label_frame, textvariable=self.tam_area_var, width=10, )
-        self.tam_area_entry.grid(row=0, column=1, padx=10, pady=5)
+        self.tam_area_entry.grid(row=0, column=1, padx=10, pady=5, columnspan=3, sticky='nsew')
         
-        # add item button ----
-        self.add_item_button = ttk.Button(self.label_frame, text='+', command=self.add_item_button_clicked)
-        self.add_item_button.config(width=2)
-        self.add_item_button.grid(row=2, column=3, padx=1, pady=1)
-      
         
         # label x (Este)
-        self.label_tam_area = ttk.Label(self.label_frame, text='Este: ')
-        self.label_tam_area.grid(row=2, column=0)
+        self.label_tam_area = ttk.Label(self.lb_coordenadas, text='Este: ')
+        self.label_tam_area.grid(row=0, column=0)
         
         # entry x
         self.x_var = tk.StringVar()
-        self.x_entry = ttk.Entry(self.label_frame, textvariable=self.x_var, width=10)
-        self.x_entry.grid(row=2, column=1, padx=10, pady=5)
+        self.x_entry = ttk.Entry(self.lb_coordenadas, textvariable=self.x_var, width=5)
+        self.x_entry.grid(row=0, column=1, padx=2, pady=5)
         
         # label y (Norte)
-        self.label_tam_area = ttk.Label(self.label_frame, text='Norte: ')
-        self.label_tam_area.grid(row=3, column=0)
+        self.label_tam_area = ttk.Label(self.lb_coordenadas, text='Norte: ')
+        self.label_tam_area.grid(row=0, column=2 )
         
         # entry y
         self.y_var = tk.StringVar()
-        self.y_entry = ttk.Entry(self.label_frame, textvariable=self.y_var, width=10)
-        self.y_entry.grid(row=3, column=1, padx=10, pady=5)
+        self.y_entry = ttk.Entry(self.lb_coordenadas, textvariable=self.y_var, width=5)
+        self.y_entry.grid(row=0, column=3, padx=2, pady=5)
         
+        self.s = ttk.Style(self.lb_coordenadas)
+        #self.s.theme_use('default')
+        self.s.configure('flat.TButton', borderwidth=0)
+        
+        # add item button
+        self.img_add = tk.PhotoImage(file='plus.png')
+        self.add_item_button = ttk.Button(self.lb_coordenadas, image=self.img_add, command=self.add_item_button_clicked,
+                                          style='flat.TButton')
+        self.add_item_button.config(width=0)
+        self.add_item_button.grid(row=0, column=4, padx=0, pady=0)
         # treeview -------------------------------------------------------------
         # define columns
         self.columns = ('1', '2', '3')
@@ -74,17 +82,17 @@ class View(ttk.Frame):
         self.tree.heading('2', text='Este')
         self.tree.heading('3', text='Norte')
         
-        self.tree.grid(row=1, column=0, sticky='nsew')
+        self.tree.grid(row=1, column=0, padx=0, pady=5, sticky='nsew')
 
         # add a scrollbar
         self.scrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL, command=self.tree.yview)
         self.tree.configure(yscroll=self.scrollbar.set)
-        self.scrollbar.grid(row=1, column=2, sticky='ns')
+        self.scrollbar.grid(row=1, column=1, pady=5, sticky='nsew')
         # ----------------------------------------------------------------------
         # data buttons ---------------------------------------------------------
         # labe frame data controllers buttons
         self.lb_data_controllers = ttk.LabelFrame(self, text='Controles')
-        self.lb_data_controllers.grid(column=0, row=2, padx=10)
+        self.lb_data_controllers.grid(column=0, row=2, padx=0, pady=10 , columnspan=2, sticky='nsew')
         
         # import button
         self.save_button = ttk.Button(self.lb_data_controllers, text='Importar', command=self.save_button_clicked)
@@ -127,8 +135,15 @@ class View(ttk.Frame):
         colors = np.random.uniform(15, 255, len(x))
 
         # plot
+        
         fig, ax = plt.subplots()
         fig.suptitle('Graficas Ubicaciones')
+        fig.set_facecolor('#F0F0F0')
+        
+        # fig plt style sean-whitegrid
+        plt.style.use('seaborn-whitegrid')
+        
+        
         
         ax.scatter(x, y, s=sizes, c=colors, vmin=0, vmax=50)
 
